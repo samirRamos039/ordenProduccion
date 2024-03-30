7688e07b2a31aa6c9cc8290c4ca135ba19ac3e27
rzagza039@gmail.com <rzagza039@gmail.com>
2024-03-30 15:27:51 -0500

    second commit

---
+++ src/main/java/com/example/produccion/ProduccionApplication.java
@@ -0,0 +1,13 @@
+package com.example.produccion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProduccionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProduccionApplication.class, args);
+	}
+
+}
