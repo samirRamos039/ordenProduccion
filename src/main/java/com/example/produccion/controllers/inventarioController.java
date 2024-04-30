262ad563fc12020eef01cb206223b70d9eb658d3
samir <rzagza039@gmail.com>
2024-04-29 21:33:30 -0500

    se corrige error 401

--- src/main/java/com/example/produccion/controllers/inventarioController.java
+++ src/main/java/com/example/produccion/controllers/inventarioController.java
@@ -5,9 +5,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.example.produccion.models.inventario;
 import com.example.produccion.services.inventarioService;
 
+@RestController
+@RequestMapping("/inventario")
 public class inventarioController {
 
     @Autowired
