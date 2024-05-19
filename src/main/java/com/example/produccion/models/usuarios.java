59bf7c471d534b0976023d2cb4b383a2b42a7b7c
rzagza039@gmail.com <rzagza039@gmail.com>
2024-05-19 17:41:53 -0500

    correccion security

--- src/main/java/com/example/produccion/models/usuarios.java
+++ src/main/java/com/example/produccion/models/usuarios.java
@@ -14,6 +14,7 @@ import lombok.*;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class usuarios {
     
     @Id
--- src/main/java/com/example/produccion/security/SecurityConfig.java
+++ src/main/java/com/example/produccion/security/SecurityConfig.java
@@ -3,16 +3,25 @@ package com.example.produccion.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
 	@Bean 	
-	public SecurityFilterChain authorizationServerSecurityFilterChain(HttpSecurity HttpSecurity)
+	public SecurityFilterChain SecurityFilterChain(HttpSecurity HttpSecurity)
 			throws Exception {
 				return HttpSecurity
 				 .csrf(config -> config.disable())
@@ -24,31 +33,33 @@ public class SecurityConfig {
 					session.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 				 })
 				 .httpBasic(null)
-				 .and()
+				 
 				 .build();
 	}
 
 	@Bean
-	UserDetailServer UserDetailServer(){
-		InMemoryDetailsManager manager = InMemoryDetailsManager();
+	UserDetailsService UserDetailsService(){
+		InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
 		manager.createUser(User.withUsername("samir")
 		    .password("1234")
-			.role()
+			.roles()
+			
 			.build());
 
 		return manager;	
 	}
-    @Bean
-	PaswordEncoder paswordEncoder(){
+    
+	@Bean
+	PasswordEncoder PasswordEncoder(){
         return NoOpPasswordEncoder.getInstance();
 	}
 
     @Bean
-	AuthenticationManager autenticationManager(HttpSecurity httpSecurity, PaswordEncoder paswordEncoder)throws Exception{
-		retrurn httpSecurity.getSharedObject(AuthenticationMangerBuilder.class)
-		         .userDetailsService(userDetailsService())
-				 .paswordEncoder(paswordEncoder)
+	AuthenticationManager AuthenticationManager(HttpSecurity httpSecurity, PasswordEncoder paswordEncoder)throws Exception{
+		return httpSecurity.getSharedObject(AuthenticationManagerBuilder.class)
+		         .userDetailsService(UserDetailsService())
+				 .passwordEncoder(paswordEncoder)
 				 .and()
-				 .build()
+				 .build();
 	}
 }
