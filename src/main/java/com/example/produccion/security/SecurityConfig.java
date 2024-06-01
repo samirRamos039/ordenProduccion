60f76d7567cd886a87d95ce83532f82ac1a8bc0c
rzagza039@gmail.com <rzagza039@gmail.com>
2024-05-31 19:28:46 -0500

    kkkk

--- src/main/java/com/example/produccion/security/SecurityConfig.java
+++ src/main/java/com/example/produccion/security/SecurityConfig.java
@@ -1,25 +1,26 @@
 package com.example.produccion.security;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-    @Aitowired
+    @Autowired
 	private UserDS userDS;
 	@Bean 	
 	public SecurityFilterChain SecurityFilterChain(HttpSecurity HttpSecurity)
@@ -33,7 +34,7 @@ public class SecurityConfig {
 				 .sessionManagement(session -> {
 					session.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 				 })
-				 .httpBasic(Cuatomizer.whitDefaults())
+				 .httpBasic(Customizer.withDefaults())
 				 
 				 .build();
 	}
